e2e92d88d77c27cdde706cbaecae6e26046e7a79
FlorentLeBot <florent.lebot.pro@gmail.com>
2022-11-02 10:53:08 +0100

    header mobile

--- head.php
+++ head.php
@@ -8,7 +8,7 @@
 
     <!-- Feuille de style css -->
     <link rel="stylesheet" href="./css/style.css">
-    <title>Titre du site</title>
+    <title>Best Music</title>
     <!-- Font awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />  
 </head>
--- header.php
+++ header.php
@@ -1,5 +1,49 @@
 <body>
     <!-- LE HEADER -->
-    <header class="container">
+    <header id="main-header" class="container">
+        <div class="menu-wrap">
+            <input type="checkbox" class="toggler " />
+            <div class="hamburger">
+                <!-- div permettant de creer les lignes du burger -->
+                <div></div>
+            </div>
+            <!-- le menu -->
+            <div class="menu">
+                <div class="menu-container">
+                    <!-- menu principal -->
+                    <nav id="main-menu">
+                        <ul>
+                            <li><a href="#">Nos Sélections</a></li>
+                            <li><a href="#">Genres Musicaux</a></li>
+                            <li><a href="blog.php">Nos Actualités</a></li>
+                            <li><a href="contact.php">Notre Blog</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+
+        <h1 class="name-site">Best Music</h1>
+
+        <!-- contenu du header -->
+
+        <div id="content-header" class="container">
+            <img class="logo-site" src="images/best-music.png" alt="image d'un vinyle">
+
+            <!-- les boutons du menu -->
+
+            <ul id="btns-header">
+                <li class="btn"><a href="#"><i class="fa-solid fa-share"></i></a></li>
+                <li class="btn"><a href="#"><i class="fa-regular fa-heart"></i></a></li>
+                <li class="btn"><a href="#"><i class="fa-solid fa-envelope"></i></a></li>
+                <li class="btn"><a href="#"><i class="fa-solid fa-user"></i></a></li>
+                <li class="btn"><a href="#"><i class="fa-solid fa-cart-shopping"></i></a></li>
+            </ul>
+
+            
+            <input type="text" id="search" class="search" placeholder="Rechercher un artiste, un titre">
+            <label for="search"><i class="fa-solid fa-magnifying-glass"></i></label>
+        </div>
         
     </header>
+    
